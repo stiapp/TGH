962c0594ca8bb9fc1bf693e8709ea48b5bc992cd
Jan_Specian <jan.specian@tul.cz>
2017-04-13 19:13:19 +0200

    promin za ten bordel

--- TGH/Uzel.java
+++ TGH/Uzel.java
@@ -1,14 +1,31 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+    Konvence je - vsecky komentare, nazvy metod a promennych v CESKEM jazyce.
+    Metody i promenne se neboj pojmenovat takto:
+    MetodaCoDelaKouzelneHodnoty(int id_hladoveho_zvirete_v_zoo)
+    Díky :)
  */
 package idos.TGH;
 
-/**
+/** třída reprezentuje uzel grafu
  *
  * @author King
  */
 public class Uzel {
+    
+    //dvojice identifikujici jednoznacne dany uzel
+    protected int cislo_stanice;
+    protected int cas;
+    
+    public Uzel(int cislo_stanice, int cas_stanice)
+    {
+        boolean chyba = false;
+        chyba = (cislo_stanice < 0 || cas_stanice < 0 || cas_stanice >= 1440);
+        //1440 pocet minut od zacatku dne - ze zadani
+        if(chyba)
+            throw new IllegalArgumentException("");
+        
+        this.cas = cas_stanice;
+        this.cislo_stanice = cislo_stanice;
+    }
     
 }
